3441a3a7dab75b6961457a96af3f39b293eeb3e4
Your Name <larry.mutuku@smart.local>
2022-06-12 13:50:09 +0300

    inital setups and user card component styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // <-- import required for collapsible rows
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserCardComponent } from './user-card/user-card.component';
+import { UserListComponent } from './user-list/user-list.component';
+
+// Import collapsible library
+import { CollapsibleModule } from 'angular2-collapsible';
+
+// Import Font Awesome
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'; 
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserCardComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,  // <-- include required BrowserAnimationsModule
+    CollapsibleModule, FontAwesomeModule // <-- include angular2-collapsible module
   ],
   providers: [],
   bootstrap: [AppComponent]
